8184922f300194bb413a336beddee8fd63f318b9
Justin Solo <justinpaulosolo@gmail.com>
2023-09-14 23:18:32 -0700

    keymap

--- nvim/.config/nvim/lua/justinsolo/keymaps.lua
+++ nvim/.config/nvim/lua/justinsolo/keymaps.lua
@@ -14,3 +14,5 @@ vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnos
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 
+vim.api.nvim_set_keymap("n", "<space>fb", ":Telescope file_browser path=%:p:h select_buffer=true<CR>",{ noremap = true })
+
