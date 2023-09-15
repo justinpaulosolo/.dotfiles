9b76891eb1df3433a9993f1fe0a19c55d742f510
Justin Solo <justinpaulosolo@gmail.com>
2023-09-14 23:26:03 -0700

    Indent line config

--- nvim/.config/nvim/lua/justinsolo/plugins.lua
+++ nvim/.config/nvim/lua/justinsolo/plugins.lua
@@ -112,12 +112,6 @@ require('lazy').setup({
   {
     -- Add indentation guides even on blank lines
     'lukas-reineke/indent-blankline.nvim',
-    -- Enable `lukas-reineke/indent-blankline.nvim`
-    -- See `:help indent_blankline.txt`
-    opts = {
-      char = '┊',
-      show_trailing_blankline_indent = false,
-    },
   },
 
   -- "gc" to comment visual regions/lines
