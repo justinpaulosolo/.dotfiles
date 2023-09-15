2e1ac4ecc98b2ed19fdfe204ccf682bb90138c3d
Justin Solo <justinpaulosolo@gmail.com>
2023-09-14 23:27:12 -0700

    lualine config

--- nvim/.config/nvim/lua/justinsolo/plugins.lua
+++ nvim/.config/nvim/lua/justinsolo/plugins.lua
@@ -98,15 +98,6 @@ require('lazy').setup({
   {
     -- Set lualine as statusline
     'nvim-lualine/lualine.nvim',
-    -- See `:help lualine.txt`
-    opts = {
-      options = {
-        icons_enabled = false,
-        theme = 'onedark',
-        component_separators = '|',
-        section_separators = '',
-      },
-    },
   },
 
   {
