4534fc9414542dfba56aec6910d69da27b500715
JustinPauloSolo <JustinPauloSolo@gmail.com>
2024-01-21 13:17:30 -0800

    config

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -17,7 +17,7 @@ return {
     -- Adds a number of user-friendly snippets
     "rafamadriz/friendly-snippets",
 
-   {
+    {
       "j-hui/fidget.nvim",
       opts = {
         -- options
@@ -75,7 +75,6 @@ return {
 
     local servers = {
       clangd = {},
-      csharp_ls = {},
       gopls = {},
       pyright = {},
       rust_analyzer = {},
--- nvim/.config/nvim/lua/plugins/status-line.lua
+++ nvim/.config/nvim/lua/plugins/status-line.lua
@@ -10,7 +10,12 @@ return {
         icons_enabled = true,
         component_separators = { left = '|', right = '|' },
         section_separators = { left = '|', right = '|' },
-      }
+      },
+      sections = {
+        lualine_b = {'branch', 'diagnostics'},
+        lualine_x = { },
+        lualine_z = { 'filetype' },
+      },
     })
   end
 }
