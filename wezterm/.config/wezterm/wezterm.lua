47c40cb14e246e326016a114dc5bd0066c9e37f9
Justin Solo <justinpaulosolo@gmail.com>
2024-06-29 13:11:03 -0700

    init

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,27 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+	config = wezterm.config_builder()
+end
+
+config.font = wezterm.font_with_fallback({
+	"MonoLisa",
+})
+
+config.audible_bell = "Disabled"
+config.color_scheme = "tokyonight"
+config.enable_scroll_bar = false
+config.window_background_opacity = 0.98
+config.window_decorations = "TITLE | RESIZE"
+config.hide_tab_bar_if_only_one_tab = true
+
+-- Fedora bug
+config.enable_wayland = false
+
+return config
