f5fa5877efcaf0031273f0fe265dd160070976d8
Justin Solo <justinpaulosolo@gmail.com>
2024-08-03 13:47:08 -0700

    clean up

--- neovim/.config/nvim/lua/custom/plugins/colorschemes.lua
+++ neovim/.config/nvim/lua/custom/plugins/colorschemes.lua
@@ -1,9 +1,17 @@
 return {
   {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme 'catppuccin-macchiato'
+    end,
+  },
+  {
     'Mofiqul/dracula.nvim',
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme 'dracula'
+      --vim.cmd.colorscheme 'dracula'
     end,
   },
 }
