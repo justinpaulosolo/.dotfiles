c002e7753b92a5f2f7614ac462cdab3beef36587
Justin Solo <justinpaulosolo@gmail.com>
2024-08-10 23:48:17 -0700

    themes

--- neovim/.config/nvim/lua/custom/plugins/colorschemes.lua
+++ neovim/.config/nvim/lua/custom/plugins/colorschemes.lua
@@ -1,20 +1,40 @@
 return {
   {
+    'olimorris/onedarkpro.nvim',
+    priorit = 1000,
+    config = function()
+      -- vim.cmd.colorscheme 'onedark'
+    end,
+  },
+  {
     'catppuccin/nvim',
     name = 'catppuccin',
-    priority = 1000,
     config = function()
       require('catppuccin').setup {
         transparent_background = true,
       }
-      vim.cmd.colorscheme 'catppuccin-macchiato'
+      --vim.cmd.colorscheme 'catppuccin-macchiato'
     end,
   },
   {
     'Mofiqul/dracula.nvim',
     priority = 1000,
     config = function()
-      --vim.cmd.colorscheme 'dracula'
+      vim.cmd.colorscheme 'dracula'
+    end,
+  },
+  {
+    'sainnhe/sonokai',
+    priority = 1000,
+    config = function()
+      -- vim.cmd.colorscheme 'sonokai'
+    end,
+  },
+  {
+    'rebelot/kanagawa.nvim',
+    priority = 1000,
+    config = function()
+      -- vim.cmd.colorscheme 'kanagawa-wave'
     end,
   },
 }
