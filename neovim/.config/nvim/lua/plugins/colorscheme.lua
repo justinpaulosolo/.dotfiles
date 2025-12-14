7fd6adfc1a6e64635cf7749f42b73f5a4957117f
Justin Solo <justin@j-s.sh>
2025-12-14 12:51:29 -0800

    chore: more config

--- neovim/.config/nvim/lua/plugins/colorscheme.lua
+++ neovim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,13 +1,12 @@
 return {
-    "sainnhe/gruvbox-material",
+	"adibhanna/yukinord.nvim",
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
-      -- load the colorscheme here
-	  vim.cmd([[let g:gruvbox_material_background = 'hard']])
-      vim.cmd([[let g:gruvbox_material_better_performance = 1]])
-
-      vim.o.background = "dark"
-      vim.cmd([[colorscheme gruvbox-material]])
+		require("yukinord").setup({
+			transparent = true,         -- Enable transparent background
+			transparent_sidebar = true, -- Enable transparent background for sidebars (NvimTree, terminal, etc.)
+		})
+      vim.cmd([[colorscheme yukinord]])
     end,
 }
