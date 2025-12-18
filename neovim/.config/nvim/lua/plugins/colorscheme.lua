68f1239a112ffbcb5414a4cbf2b8d7619b9f9c3c
Justin Solo <justin@j-s.sh>
2025-12-17 22:45:14 -0800

    chore: new colorscheme

--- neovim/.config/nvim/lua/plugins/colorscheme.lua
+++ neovim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,25 @@
 return {
-	"adibhanna/yukinord.nvim",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-		require("yukinord").setup({
-			transparent = true,         -- Enable transparent background
-			transparent_sidebar = true, -- Enable transparent background for sidebars (NvimTree, terminal, etc.)
-		})
-      vim.cmd([[colorscheme yukinord]])
-    end,
+	{
+		"adibhanna/yukinord.nvim",
+		lazy = true, -- make sure we load this during startup if it is your main colorscheme
+		priority = 1000, -- make sure to load this before all the other start plugins
+		config = function()
+			require("yukinord").setup({
+				transparent = true,         -- Enable transparent background
+				transparent_sidebar = true, -- Enable transparent background for sidebars (NvimTree, terminal, etc.)
+			})
+		  -- vim.cmd([[colorscheme yukinord]])
+		end,
+	},
+	{
+		"scottmckendry/cyberdream.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("cyberdream").setup({
+				saturation = 0.6,
+			})
+		    vim.cmd([[colorscheme cyberdream]])
+		end,
+	}
 }
